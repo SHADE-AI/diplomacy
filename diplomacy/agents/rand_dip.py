2ce975f0281eec4dc147aeeee2a05eccdb29bf2f
Kartik Shenoy <kartikshenoy@Kartiks-MacBook-Pro.local>
2022-03-22 12:21:35 -0700

    minor change

--- diplomacy/agents/rand_dip.py
+++ diplomacy/agents/rand_dip.py
@@ -1,13 +1,13 @@
 import asyncio
 import random
 
-# from diplomacy.agents.baseline_bots.loyal_bot import LoyalBot
-# from diplomacy.agents.baseline_bots.pushover_bot import PushoverBot
-# # from diplomacy.agents.baseline_bots.random_allier_proposer_bot import RandomAllierProposerBot
-# from diplomacy.agents.baseline_bots.random_honest_bot import RandomHonestBot
-# from diplomacy.agents.baseline_bots.random_honest_order_accepter_bot import RandomHonestAccepterBot
-from agents.baseline_bots.random_proposer_bot import RandomProposerBot
-# from diplomacy.agents.baseline_bots.random_support_proposer_bot import RandomSupportProposerBot
+from diplomacy.agents.baseline_bots.loyal_bot import LoyalBot
+from diplomacy.agents.baseline_bots.pushover_bot import PushoverBot
+# from diplomacy.agents.baseline_bots.random_allier_proposer_bot import RandomAllierProposerBot
+from diplomacy.agents.baseline_bots.random_honest_bot import RandomHonestBot
+from diplomacy.agents.baseline_bots.random_honest_order_accepter_bot import RandomHonestAccepterBot
+from diplomacy.agents.baseline_bots.random_proposer_bot import RandomProposerBot
+from diplomacy.agents.baseline_bots.random_support_proposer_bot import RandomSupportProposerBot
 from diplomacy.client.connection import connect
 from diplomacy.utils import exceptions
 import argparse
